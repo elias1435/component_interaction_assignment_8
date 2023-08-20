5963ff2a9bc4cb981ae3eb53eeb234b21cfd0116
elias1435 <elias1435@gmail.com>
2023-08-20 17:06:53 +0600

    first commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+   const props  = defineProps(['message']);
+   const emit   = defineEmits(['update:modelValue']);
+
+   function messageEmitted (){
+     props.name = emit('update:message','Hi I am a Vue Learner!')
+   }
+
+</script>
+
+<template>
+    <div class="mt-10 h-96 bg-gradient-to-r from-blue-100 to-teal-100 flex items-center justify-center flex-col">
+        <h1 class="text-black-900 text-xl my-5">Chiled Component:</h1>
+        <h2 class="text-black text-xl my-5">Message : <span class="text-red-600 hover:text-blue-900">{{ message }}</span></h2>
+    </div>
+
+    <div class="flex items-center justify-center">
+        <button @click="messageEmitted ()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded"><em>Button</em></button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { reactive,ref } from 'vue'
+    import ChildComponent from './ChildComponent.vue';
+
+    const persons = reactive({
+            'name'  : 'Md. Eliass Hossain',
+            'profession' : 'Web Developer @upwork.com',
+            'projects' : [
+                {'name' : 'Blog', 'link' : 'www.howtoonlinetips.com'},
+                {'name' : 'Web Project', 'link' : 'www.ppa.co.uk'}
+            ],
+            'message'   : 'Working as a web developer with 10 years of experience'
+        });
+</script>
+
+<template>
+    <div class="h-96 bg-gradient-to-r from-blue-100 to-pink-100 flex items-center justify-center flex-col">
+        <h1 class="text-black-900 text-xl my-5">Parent Component:</h1>
+        <h2 class="text-black text-l ">Name : {{ persons.name }}</h2>
+        <h2 class="text-black text-l ">Profession : {{ persons.profession }}</h2>
+        <h2 class="text-black text-l ">Message : {{ persons.message }}</h2>
+        <h2 class="text-black text-l mt-5 mb-3">Projects:</h2>
+        <ul class="list-disc">
+            <li class="ml-5" v-for="(project, index) in persons.projects" :key="index">{{ project.name }}</li>
+        </ul>
+    </div>
+
+    <ChildComponent v-model:message="persons.message" ></ChildComponent>
+</template>
+
+<style scoped>
+
+</style>
